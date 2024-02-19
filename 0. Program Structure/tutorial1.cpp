04e066640256ff1d1c724770e01ceb6211c969f4
Joseph Webster <rwc.webster@gmail.com>
2024-02-19 17:29:43 +0000

    program structure init

---
+++ 0. Program Structure/tutorial1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>  // This line imports the iostram Library
+using namespace std; // Specifies the namespace
+
+int main() {                      // Program Entry Point
+  cout << "Hello World!" << endl; // Print Hello World! To Console
+
+  return 0; // (it is not neccesary in the entry point) functions must return a value of the indicated function type.
+}
